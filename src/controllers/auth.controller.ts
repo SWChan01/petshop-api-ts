f80a60d7d9400398249e3f8f9396ab44cd39224c
Hanna Seraphim <hannaseraphim@gmail.com>
2025-10-05 03:24:18 -0400

    Updates Error JSON model

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -7,18 +7,18 @@ const saltRounds = 10;
 export const signUp = (req: Request, res: Response) => {
     const {name, email, password} = req.body;
         db.query('SELECT * FROM users WHERE email = ?', [email], async (err, results) => {
-            if(err) res.status(500).json({Error: err.message, Cause: err.cause, Where: err.code});
+            if(err) res.status(500).json({Message: err.message, Cause: err.cause, Where: err.code});
                 
             if(results.length > 0) {return res.status(402).json({Message: 'USER_ALREADY_EXISTS'});} 
             else {
                 try {
                     const hashedPassword = await bcrypt.hash(password, saltRounds);
                     db.query('INSERT INTO users(name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword], (err, result) => {
-                        if(err) res.status(500).json({Error: 'INTERNAL_ERROR', Cause: err.cause, Where: err.code});
+                        if(err) res.status(500).json({Message: 'INTERNAL_ERROR', Cause: err.cause, Where: err.code});
                         return res.status(201).json({Message: 'USER_CREATED', Content: result});
                 });
                 } catch (err) {
-                    res.status(500).json({Error: 'Error while trying to cryptograph password.'});
+                    res.status(500).json({Message: 'Error while trying to cryptograph password.'});
             }
         }
     })
